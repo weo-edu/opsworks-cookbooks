668e89c6d9fb1316fa396cd7d67246acb82f40aa
Andrew Shaffer <andrew@weo.io>
2015-02-20 15:48:15 -0800

    update

--- opsworks_nodejs/recipes/sync.rb
+++ opsworks_nodejs/recipes/sync.rb
@@ -1,4 +1,4 @@
 node[:deploy].each do |application, deploy|
-  cmd = "source #{deploy[:deploy_to]}/shared/app.env && cd #{deploy[:current_path]} && npm run sync"
+  cmd = "source #{deploy[:deploy_to]}/shared/app.env; cd #{deploy[:current_path]}; /usr/local/bin/npm run sync"
   Chef::Log.info(OpsWorks::ShellOut.shellout(cmd))
 end
