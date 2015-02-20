e551c6eff08faeda38361239bc35b03675231397
Andrew Shaffer <andrew@weo.io>
2015-02-20 15:29:24 -0800

    update

--- opsworks_nodejs/recipes/sync.rb
+++ opsworks_nodejs/recipes/sync.rb
@@ -1,4 +1,4 @@
 node[:deploy].each do |application, deploy|
   Chef::Log.info("TEST TEST #{application} asdf #{deploy}")
-  Chef::Log.info(OpsWorks::ShellOut.shellout("source #{deploy[:deploy_t]}/shared/app.env; echo $MONGO_URL"))
+  Chef::Log.info(OpsWorks::ShellOut.shellout("source #{deploy[:deploy_to]}/shared/app.env; echo $MONGO_URL"))
 end
